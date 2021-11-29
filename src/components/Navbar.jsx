3ecd40ffb143887c8e1613e9e81ccb541d92e740
Ondrej Futera <ondra.futera@gmail.com>
2021-11-29 17:29:06 +0100

    Re-add navbar, fix Intro backround-image.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
+import "./navbar.scss";
+import Logo from '../images/logo.gif'
+
 const Navbar = () => {
     return (
-      <div>
-        Navbar
+      <div className='navbar'>
+        <img src={Logo} className='logo' alt='logo'/>
+        <div className='navbar-menu'>
+          <span><a href="https://google.com">home</a></span>
+          <span><a href="https://google.com">nieuws</a></span>
+          <span><a href="https://google.com">verhalen</a></span>
+          <span><a href="https://google.com">sportclubs</a></span>
+          <span><a href="https://google.com">partners</a></span>
+          <span><a href="https://google.com">pride in nl</a></span>
+          <span><a href="https://google.com">overig</a></span>
+        </div>
       </div>
     );
   };
